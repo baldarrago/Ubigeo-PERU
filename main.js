fea7307b77babc0fc11eb9b90c9b09097ff205d5
unknown <baldarragorom@gmail.com>
2021-04-03 17:36:17 -0500

    js vanilla

---
+++ main.js
@@ -0,0 +1,47 @@
+$departamento = document.querySelector("#departamento")
+$provincia = document.querySelector("#provincia")
+$distrito = document.querySelector("#distrito")
+
+$ubigeo = document.querySelector("#ubigeo")
+
+ubigeo = new Ubigeo()
+
+//Carga Inicial
+$departamento.innerHTML+= ubigeo.getDepartamento().map((dep) => {
+    return `<option value="${dep.codDep}">${dep.desDep}</option>`
+})
+$provincia.innerHTML+= ubigeo.getProvincia().map((prov) => {
+    return `<option value="${prov.codProv}">${prov.desProv}</option>`
+})
+$distrito.innerHTML+= ubigeo.getDistrito().map((dis) => {
+    return `<option value="${dis.codDist}">${dis.desDist}</option>`
+})
+
+$ubigeo.innerHTML = $distrito.value + " - " + $departamento.options[$departamento.selectedIndex].text + " - " + $provincia.options[$provincia.selectedIndex].text + " - " + $distrito.options[$distrito.selectedIndex].text
+
+//Eventos
+$departamento.addEventListener('change', function() {
+    $provincia.innerHTML= ""
+    $provincia.innerHTML+= ubigeo.getProvincia(this.value).map((prov) => {
+        return `<option value="${prov.codProv}">${prov.desProv}</option>`
+    })
+    $distrito.innerHTML= ""
+    $distrito.innerHTML+= ubigeo.getDistrito(this.value+"01").map((dis) => {
+        return `<option value="${dis.codDist}">${dis.desDist}</option>`
+    }) 
+
+    $ubigeo.innerHTML = $distrito.value + " - " + $departamento.options[$departamento.selectedIndex].text + " - " + $provincia.options[$provincia.selectedIndex].text + " - " + $distrito.options[$distrito.selectedIndex].text
+})
+
+$provincia.addEventListener('change', function() {
+    $distrito.innerHTML= ""
+    $distrito.innerHTML+= ubigeo.getDistrito(this.value).map((dis) => {
+        return `<option value="${dis.codDist}">${dis.desDist}</option>`
+    })
+    
+    $ubigeo.innerHTML = $distrito.value + " - " + $departamento.options[$departamento.selectedIndex].text + " - " + $provincia.options[$provincia.selectedIndex].text + " - " + $distrito.options[$distrito.selectedIndex].text
+})
+
+$distrito.addEventListener('change', function(){
+    $ubigeo.innerHTML = $distrito.value + " - " + $departamento.options[$departamento.selectedIndex].text + " - " + $provincia.options[$provincia.selectedIndex].text + " - " + $distrito.options[$distrito.selectedIndex].text
+})
